71748f083f6fc2228f8ffd5cbf9f2ce9bfaaa47e
Greg Chetcuti <greg@chetcuti.com>
2018-04-13 15:47:08 -0700

    Change location of log file

--- _includes/init.inc.php
+++ _includes/init.inc.php
@@ -1,4 +1,4 @@
 <?php
 define('DIR_ROOT', dirname(dirname(__FILE__)));
 define('DIR_INC', DIR_ROOT . '/_includes');
-@ini_set("error_log", DIR_ROOT . "/gjclasses.log");
+@ini_set("error_log", DIR_ROOT . "/_dev/gjclasses.log");
