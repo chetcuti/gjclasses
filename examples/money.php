de2ef30e3333256d3dc4adc719d4768b85fb8ea5
Greg Chetcuti <greg@chetcuti.com>
2018-04-13 13:34:31 -0700

    Add Money class example

---
+++ examples/money.php
@@ -0,0 +1,10 @@
+<?php
+require_once __DIR__ . '/../_includes/init.inc.php';
+require_once DIR_INC . '/config.inc.php';
+
+require_once DIR_ROOT . '/vendor/autoload.php';
+
+$_SESSION['s_user_id'] = 23;
+$money = new GJClasses\Money();
+
+echo $money->getConvRate('USD', 'CAD');
