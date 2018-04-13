d8922c8ae74531f872ed9fe66d195f11b845dd94
Greg Chetcuti <greg@chetcuti.com>
2018-04-13 00:40:26 -0700

    Add Log class example

---
+++ examples/Log.php
@@ -0,0 +1,12 @@
+<?php
+require_once __DIR__ . '/../_includes/init.inc.php';
+require_once DIR_INC . '/config.inc.php';
+
+require_once DIR_ROOT . '/vendor/autoload.php';
+
+$_SESSION['s_user_id'] = 23;
+$log = new GJClasses\Log('/examples/Log.php');
+
+$message = 'This is the log message';
+$extra_info = array('Extra' => 'data', 'Goes' => 'here');
+$log->warning($message, $extra_info);
