2229372260849ce5b7d4ada0dc54df6e269cc11f
Greg Chetcuti <greg@chetcuti.com>
2018-09-10 14:10:23 -0700

    Add DIR_TEMP declaration to init.inc.php

--- _includes/init.inc.php
+++ _includes/init.inc.php
@@ -1,4 +1,5 @@
 <?php
 define('DIR_ROOT', dirname(dirname(__FILE__)));
 define('DIR_INC', DIR_ROOT . '/_includes');
+define('DIR_TEMP', DIR_ROOT . '/temp');
 @ini_set("error_log", DIR_ROOT . "/_dev/gjclasses.log");
