401b211a02f6c73abde35efbd4c426b3646080ba
Greg Chetcuti <greg@chetcuti.com>
2018-10-26 17:38:13 -0700

    Rename named constants in init.inc.php
    To avoid potential conflicts with other packages.

--- _includes/init.inc.php
+++ _includes/init.inc.php
@@ -1,5 +1,5 @@
 <?php
-define('DIR_ROOT', dirname(dirname(__FILE__)));
-define('DIR_INC', DIR_ROOT . '/_includes');
-define('DIR_TEMP', DIR_ROOT . '/temp');
-@ini_set("error_log", DIR_ROOT . "/_dev/gjclasses.log");
+define('GJC_DIR_ROOT', dirname(dirname(__FILE__)));
+define('GJC_DIR_INC', GJC_DIR_ROOT . '/_includes');
+define('GJC_DIR_TEMP', GJC_DIR_ROOT . '/temp');
+@ini_set("error_log", GJC_DIR_ROOT . "/_dev/gjclasses.log");
--- examples/money.php
+++ examples/money.php
@@ -1,8 +1,8 @@
 <?php
 require_once __DIR__ . '/../_includes/init.inc.php';
-require_once DIR_INC . '/config.inc.php';
+require_once GJC_DIR_INC . '/config.inc.php';
 
-require_once DIR_ROOT . '/vendor/autoload.php';
+require_once GJC_DIR_ROOT . '/vendor/autoload.php';
 
 $_SESSION['s_user_id'] = 23;
 $money = new GJClasses\Money();
