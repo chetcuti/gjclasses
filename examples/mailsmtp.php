ee09fe744c6c6c9883cb2538aefc8db446496584
Greg Chetcuti <greg@chetcuti.com>
2018-10-27 14:14:42 -0700

    Update MailSmtp class example

--- examples/mailsmtp.php
+++ examples/mailsmtp.php
@@ -4,10 +4,6 @@ require_once GJC_DIR_INC . '/config.inc.php';
 
 define('EMAIL_ENCODING_TYPE', 'UTF-8');
 define('SMTP_MAIL_SERVER', 'mail.example.com');
-define('SMTP_FROM_NAME', 'Admin');
-define('SMTP_FROM_ADDRESS', 'mail@example.com');
-define('SMTP_REPLY_NAME', 'Admin');
-define('SMTP_REPLY_ADDRESS', 'mail@example.com');
 define('SMTP_AUTHENTICATION_ON', true);
 define('SMTP_USERNAME', 'mail@example.com');
 define('SMTP_PASSWORD', '@cc0untp@55w0rd');
@@ -16,13 +12,16 @@ define('SMTP_PORT', 587);
 
 require_once GJC_DIR_ROOT . '/vendor/autoload.php';
 
-$smtp = new GJClasses\MailSmtp();
-
+$from_name = "From Name";
+$from_address = "from@example.com";
+$reply_name = "Reply Name";
+$reply_address = "reply@example.com";
 $recipients = array('address1@example.com', 'address2@example.com');
 $subject = "Example Subject";
 $message_html = 'This is the HTML message.';
 $message_text = 'This is the TEXT message.';
 
-$message = $smtp->send($recipients, $subject, $message_html, $message_text);
+$smtp = new GJClasses\MailSmtp();
+$message = $smtp->send($from_name, $from_address, $reply_name, $reply_address, $recipients, $subject, $message_html, $message_text);
 
 echo $message;
