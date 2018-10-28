722526a991c8afe949f39ebdb854b90969f1abfa
Greg Chetcuti <greg@chetcuti.com>
2018-10-28 08:26:12 -0700

    Update MailPhp class example

--- examples/mailphp.php
+++ examples/mailphp.php
@@ -1,4 +1,9 @@
 <?php
+/*
+ * If you may want to switch between using SMTP and not using SMTP, you should implement the Mail class instead of this
+ * one. The Mail class allows you to switch sending methods with a flip of a switch, whereas this class only allows for
+ * a single send method.
+ */
 require_once __DIR__ . '/../_includes/init.inc.php';
 require_once GJC_DIR_INC . '/config.inc.php';
 
