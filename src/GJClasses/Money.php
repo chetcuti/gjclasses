0aaad24acc9999f6adaf30d41204f306533f27ad
Greg Chetcuti <greg@chetcuti.com>
2019-02-08 22:40:35 -0800

    Update Money class

--- src/GJClasses/Money.php
+++ src/GJClasses/Money.php
@@ -3,6 +3,13 @@ namespace GJClasses;
 
 class Money
 {
+    public $source;
+
+    public function __construct($source)
+    {
+        $this->source = $source;
+    }
+
     public function convertAmount($amount, $from_currency, $to_currency)
     {
         return $this->getConvRate($from_currency, $to_currency) * $amount;
@@ -10,27 +17,29 @@ class Money
 
     public function getConvRate($from_currency, $to_currency)
     {
-        list($currency_slug, $full_url) = $this->getConvUrl($from_currency, $to_currency);
-        $remote = new Remote();
-        $result = $remote->getFileContents($full_url);
-        if ($result === false) return false;
-        $json_result = json_decode($result);
-        $conversion_rate = $json_result->{$currency_slug}->val;
+        $conversion_rate = 0.0;
 
-        if (!is_null($conversion_rate) && $conversion_rate != '') {
+        if ($this->source === 'era') {
 
-            return $conversion_rate;
+            $full_url = 'https://api.exchangeratesapi.io/latest?base=' . $from_currency . '&symbols=' . $to_currency;
+            $remote = new Remote();
+            $result = $remote->getFileContents($full_url);
+            if ($result === false) return false;
+            $json_result = json_decode($result, true);
+            $conversion_rate = $json_result['rates'][$to_currency];
 
-        } else {
+        } elseif ($this->source === 'fcca') {
 
-            return false;
+            $currency_slug = $from_currency . '_' . $to_currency;
+            $full_url = 'https://free.currencyconverterapi.com/api/v5/convert?q=' . $currency_slug . '&compact=y';
+            $remote = new Remote();
+            $result = $remote->getFileContents($full_url);
+            if ($result === false || $result === '{}') return false;
+            $json_result = json_decode($result);
+            $conversion_rate = $json_result->{$currency_slug}->val;
 
         }
-    }
 
-    public function getConvUrl($from_currency, $to_currency)
-    {
-        $currency_slug = $from_currency . '_' . $to_currency;
-        return array($currency_slug, 'https://free.currencyconverterapi.com/api/v5/convert?q=' . $currency_slug . '&compact=y');
+        return $conversion_rate;
     }
 }
