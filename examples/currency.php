e027942941104b277d366bfd347eee70c4b44356
Greg Chetcuti <greg@chetcuti.com>
2021-06-26 12:53:51 -0700

    Add support for exchangerate.host

--- examples/currency.php
+++ examples/currency.php
@@ -5,11 +5,12 @@ require_once GJC_DIR_INC . '/config.inc.php';
 require_once GJC_DIR_ROOT . '/vendor/autoload.php';
 
 // Currency Converter Source
-// era  = Exchange Rates API (http://exchangeratesapi.io)
-// er-a  = ExchangeRate-API (https://www.exchangerate-api.com)
-// fixer  = Fixer.io (https://fixer.io)
-// interzoid  = Interzoid (https://interzoid.com)
-$converter_source = 'era';
+// era  = http://exchangeratesapi.io
+// er-a  = https://www.exchangerate-api.com
+// erh = https://exchangerate.host -- NO API KEY REQUIRED!
+// fixer  = https://fixer.io
+// interzoid  = https://interzoid.com
+$converter_source = 'erh';
 
 // API Key
 $api_key = 'xxxxx';
