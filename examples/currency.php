65caad73c405ba87505a74b87057f21e401a2743
Greg Chetcuti <greg@chetcuti.com>
2024-01-03 14:49:50 -0800

    Add support for Free Currency Rates API

--- examples/currency.php
+++ examples/currency.php
@@ -7,10 +7,11 @@ require_once GJC_DIR_ROOT . '/vendor/autoload.php';
 // Currency Converter Source
 // era  = http://exchangeratesapi.io
 // er-a  = https://www.exchangerate-api.com
-// erh = https://exchangerate.host -- NO API KEY REQUIRED!
+// erh = https://exchangerate.host
+// fcra = https://github.com/fawazahmed0/currency-api -- NO API KEY REQUIRED!
 // fixer  = https://fixer.io
 // interzoid  = https://interzoid.com
-$converter_source = 'erh';
+$converter_source = 'fcra';
 
 // API Key
 $api_key = 'xxxxx';
